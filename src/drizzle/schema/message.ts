29eb9f5c1fa3d3d436ae7bac708ffc0bd1aa1554
Ruslan <50518079+n8b8dy@users.noreply.github.com>
2024-06-30 21:40:14 +0300

    Feat: Create basic drizzle schemas

---
+++ src/drizzle/schema/message.ts
@@ -0,0 +1,11 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+
+export const message = pgTable('message', {
+  id: uuid('id').primaryKey(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow().$onUpdate(() => new Date()),
+
+  name: text('name').notNull(),
+  email: text('email').notNull(),
+  message: text('message').notNull(),
+})
