742b452626bfd6ebd13b0c0c523296b51b608a73
n8b8dy <ruslanchernomorski@gmail.com>
2024-07-01 04:02:55 +0300

    Fix: Use add constraints to joint tables

--- src/drizzle/schema/project/projectsToTechnologies.ts
+++ src/drizzle/schema/project/projectsToTechnologies.ts
@@ -1,7 +1,8 @@
-import { pgTable, uuid } from 'drizzle-orm/pg-core'
+import { pgTable, unique, uuid } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+
 import { projects } from '@/drizzle/schema/project/projects'
 import { technologies } from '@/drizzle/schema/technology/technologies'
-import { relations } from 'drizzle-orm'
 
 export const projectsToTechnologies = pgTable('projects_to_technologies', {
   projectId: uuid('project_id')
@@ -10,7 +11,9 @@ export const projectsToTechnologies = pgTable('projects_to_technologies', {
   technologyId: uuid('technology_id')
     .notNull()
     .references(() => technologies.id, { onDelete: 'cascade' }),
-})
+}, (table) => ({
+  projectsToTechnologiesUniqueIds: unique('projects_to_technologies_unique_ids').on(table.projectId, table.technologyId),
+}))
 
 export const projectsToTechnologiesRelations = relations(projectsToTechnologies, ({ one }) => ({
   project: one(projects, { fields: [projectsToTechnologies.projectId], references: [projects.id] }),
