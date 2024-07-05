99bceb742deb1187658ff7f0bd7f0bc9899d0919
n8b8dy <ruslanchernomorski@gmail.com>
2024-07-05 06:45:23 +0300

    Feat: Add query support to drizzle instance

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,6 +1,13 @@
-import 'dotenv/config';
-import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import 'dotenv/config'
+import { Pool } from 'pg'
+import { drizzle } from 'drizzle-orm/node-postgres'
+
+import * as messagesSchema from '@/drizzle/schema/message/messages'
+import * as projectsSchema from '@/drizzle/schema/project/projects'
+import * as projectsToTechnologiesSchema from '@/drizzle/schema/project/projectsToTechnologies'
+import * as projectsToTagsSchema from '@/drizzle/schema/project/projectsToTags'
+import * as technologiesSchema from '@/drizzle/schema/technology/technologies'
+import * as tagsSchema from '@/drizzle/schema/tag/tags'
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -9,7 +16,15 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: false,
-});
+})
 
-export const db = drizzle(pool);
-
+export const db = drizzle(pool, {
+  schema: {
+    ...messagesSchema,
+    ...projectsSchema,
+    ...projectsToTechnologiesSchema,
+    ...projectsToTagsSchema,
+    ...technologiesSchema,
+    ...tagsSchema,
+  },
+})
