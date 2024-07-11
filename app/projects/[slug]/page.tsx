fef812dc964c3ab72484086771c9cb19aee35b1a
Ruslan <50518079+n8b8dy@users.noreply.github.com>
2024-07-11 23:01:26 +0300

    Feat: Use drizzle for projects in projects pages

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,26 +1,50 @@
+import { eq } from 'drizzle-orm'
+
 import { Section } from '@/components/layout/Section'
 import { Heading } from '@/components/typography/Heading'
 import { BreadCrumbs } from '@/components/common/BreadCrumbs'
-
-import prisma from '@/lib/prisma'
-import { cn } from '@/utils/styles'
-import { ProjectTags, ProjectTechnologies } from '@/constants'
 import { MarkdownCompiler } from '@/components/markdown/MarkdownCompiler'
 import { ErrorSection } from '@/components/layout/ErrorSection'
 
+import { cn } from '@/utils/styles'
+
+import { db } from '@/drizzle/db'
+import { projects } from '@/drizzle/schema/project/projects'
+
+import { TechnologiesIcons } from '@/constants'
+
 export async function generateStaticParams() {
-  return prisma.project.findMany({
-    select: { slug: true },
+  return db.query.projects.findMany({
+    columns: {
+      slug: true,
+    },
   })
 }
 
 async function getData(slug: string) {
-  const project = await prisma.project.findUnique({
-    where: { slug },
+  const projectData = await db.query.projects.findFirst({
+    where: eq(projects.slug, slug),
+    with: {
+      projectsToTechnologies: {
+        with: { technology: true },
+      },
+      projectsToTags: {
+        with: { tag: true },
+      },
+
+    },
   })
 
+  if (!projectData) return { project: undefined }
+
+  const { projectsToTechnologies, projectsToTags, ...project } = projectData
+
   return {
-    project,
+    project: {
+      ...project,
+      tags: projectsToTags.map(projectToTag => projectToTag.tag),
+      technologies: projectsToTechnologies.map(projectToTechnology => projectToTechnology.technology),
+    },
   }
 }
 
@@ -45,23 +69,27 @@ export default async function ProjectsSlug({ params }: {
 
       <div className={cn('mt-1 mb-2 flex items-center flex-wrap gap-x-2')}>
         <Heading tag="h2" className={cn('text-3xl sm:text-3xl md:text-3xl font-medium')} terminal>{title}</Heading>
-        {tags.map(tag => <div key={`${id}-${tag}`} className={cn('mt-1 px-3 py-1 text-sm bg-neutral-200 dark:bg-neutral-800 rounded-full')}>
-          {ProjectTags[tag]}
+        {tags.map(tag => <div
+          key={tag.slug}
+          className={cn('mt-1 px-3 py-1 text-sm bg-neutral-200 dark:bg-neutral-800 rounded-full')}
+        >
+          {tag.name}
         </div>)}
       </div>
 
       <div className={cn('flex flex-wrap gap-2')}>
-        {technologies.map(technology => <div key={`${id}-${technology}`} className={cn('px-2 py-1.5 flex gap-1.5 text-sm bg-neutral-200 dark:bg-neutral-800 rounded')}>
+        {technologies.map(technology => <div key={`${id}-${technology}`} className={cn(
+          'px-2 py-1.5 flex gap-1.5 text-sm bg-neutral-200 dark:bg-neutral-800 rounded')}>
           <div className={cn('w-5')}>
-            {ProjectTechnologies[technology].icon}
+            {TechnologiesIcons[technology.slug]}
           </div>
-          {ProjectTechnologies[technology].title}
+          {technology.name}
         </div>)}
       </div>
 
-      <div className={cn('mt-4 flex flex-col gap-1 text-lg')}>
+      {sections !== null && <div className={cn('mt-4 flex flex-col gap-1 text-lg')}>
         {sections.map((section, index) => <MarkdownCompiler key={`${id}-section-${index}`} content={section}/>)}
-      </div>
+      </div>}
     </Section>
   )
 }
