d9da1f56658cdece853a16a57284abf3060ca18d
Ruslan <50518079+n8b8dy@users.noreply.github.com>
2024-07-18 04:12:11 +0300

    Feat: Create sessions schema

---
+++ src/drizzle/schema/session/sessions.ts
@@ -0,0 +1,26 @@
+import type { InferInsertModel, InferSelectModel } from 'drizzle-orm'
+
+import { relations } from 'drizzle-orm'
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+
+import { base } from '@/drizzle/schema/helpers'
+import { users } from '@/drizzle/schema/user/users'
+
+
+export const sessions = pgTable('sessions', {
+  ...base,
+
+  userId: uuid('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
+  token: text('token').notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+})
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, {
+    fields: [sessions.userId],
+    references: [users.id],
+  }),
+}))
+
+export type Session = InferSelectModel<typeof sessions>
+export type SessionInsert = InferInsertModel<typeof sessions>
