c841d8ef6ffe4e402de7436eff2455b5b12e40b1
n8b8dy <ruslanchernomorski@gmail.com>
2024-07-29 19:13:55 +0300

    Feat: Add more icons

--- src/components/lib/svg/Node.tsx
+++ src/components/lib/svg/Node.tsx
@@ -4,20 +4,20 @@ export const NodeSVG = (props: SVGProps) => {
   return (
     <svg viewBox="0 0 128 128" {...props}>
       <path
-        fill="url(#a)"
+        fill="url(#node_a)"
         d="M66.958.825a6.07 6.07 0 0 0-6.035 0L11.103 29.76c-1.895 1.072-2.96 3.095-2.96 5.24v57.988c0 2.143 1.183 4.167 2.958 5.24l49.82 28.934a6.07 6.07 0 0 0 6.036 0l49.82-28.935c1.894-1.072 2.958-3.096 2.958-5.24V35c0-2.144-1.183-4.167-2.958-5.24z"
       ></path>
       <path
-        fill="url(#b)"
+        fill="url(#node_b)"
         d="M116.897 29.76 66.841.825A8.161 8.161 0 0 0 65.302.23L9.21 96.798a6.251 6.251 0 0 0 1.657 1.43l50.057 28.934c1.42.833 3.076 1.072 4.615.595l52.66-96.925a3.702 3.702 0 0 0-1.302-1.072z"
       ></path>
       <path
-        fill="url(#c)"
+        fill="url(#node_c)"
         d="M116.898 98.225c1.42-.833 2.485-2.262 2.958-3.81L65.066.108c-1.42-.238-2.959-.119-4.26.715L11.104 29.639l53.606 98.355c.71-.12 1.54-.358 2.25-.715z"
       ></path>
       <defs>
         <linearGradient
-          id="a"
+          id="node_a"
           x1="34.513"
           x2="27.157"
           y1="15.535"
@@ -32,7 +32,7 @@ export const NodeSVG = (props: SVGProps) => {
           <stop offset="1" stopColor="#3DAE2B"></stop>
         </linearGradient>
         <linearGradient
-          id="b"
+          id="node_b"
           x1="30.009"
           x2="50.533"
           y1="23.359"
@@ -46,7 +46,7 @@ export const NodeSVG = (props: SVGProps) => {
           <stop offset=".908" stopColor="#6ABF4B"></stop>
         </linearGradient>
         <linearGradient
-          id="c"
+          id="node_c"
           x1="21.917"
           x2="40.555"
           y1="22.261"
