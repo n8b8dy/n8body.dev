2338d8a96db0377474dcd48bf0e74a395ee6a435
Ruslan <50518079+n8b8dy@users.noreply.github.com>
2024-08-01 23:49:04 +0300

    Feat: Add ranks to technologies and domains

--- src/drizzle/schema/domain/domains.ts
+++ src/drizzle/schema/domain/domains.ts
@@ -1,6 +1,6 @@
 import type { InferInsertModel, InferSelectModel } from 'drizzle-orm'
 
-import { boolean, pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { boolean, integer, pgTable, text } from 'drizzle-orm/pg-core'
 import { relations } from 'drizzle-orm'
 
 import { base } from '@/drizzle/schema/helpers'
@@ -15,6 +15,7 @@ export const domains = pgTable('domains', {
   borderColor: text('border_color'),
 
   featured: boolean('featured').notNull().default(false),
+  rank: integer('rank').notNull().default(1)
 })
 
 export const domainsRelations = relations(domains, ({ many }) => ({
