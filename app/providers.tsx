d526949a65164c03702db3c1001688f41e34b6e0
Ruslan <50518079+n8b8dy@users.noreply.github.com>
2024-09-04 12:42:11 +0300

    Remove next-auth

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,11 @@
 import type { PropsWithChildren } from 'react'
 
-import { getServerSession } from 'next-auth'
 import { ThemeProvider } from 'next-themes'
 
-import { SessionProvider } from '@/providers/SessionProvider'
-
 export async function Providers({ children }: PropsWithChildren) {
-  const session = await getServerSession()
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <SessionProvider session={session}>{children}</SessionProvider>
+      {children}
     </ThemeProvider>
   )
 }
